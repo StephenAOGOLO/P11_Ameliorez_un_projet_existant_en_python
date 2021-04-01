76e1916e2cab4e4dc85206845134804dc243021c
StephenAOGOLO <56355715+StephenAOGOLO@users.noreply.github.com>
2021-04-01 17:32:22 +0200

    Delete the_secrets.py

--- Pure_Beurre/Pure_Beurre/the_secrets.py
+++
@@ -1,5 +0,0 @@
-""" This module provides the secret keys separately. """
-SECRET_KEY = '8-ej+n#3gc4ziju)h%7rr83e*t25g-xlm2sfi+kdjjakjg1#j9'
-GMAP_KEY = 'AIzaSyCXK2vbFQ71bwyAROMJCC86PktF-eiYOj8'
-EHP = "devdjangotest.1234"
-EHU = "devdjangotest@gmail.com"
